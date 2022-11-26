817870c7027f4ed12971e2f443d1dde233e0b5d6
HashemRadaideh <hashemalradaideh@gmail.com>
2022-11-26 18:41:04 +0300

    Prettier format.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,5 +18,5 @@ export default defineConfig({
     minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
     sourcemap: !!process.env.TAURI_DEBUG,
   },
-  plugins: [react()]
-})
+  plugins: [react()],
+});
